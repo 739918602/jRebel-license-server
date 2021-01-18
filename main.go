f33962ebfbc524a3fa728d01bd24ea2b5559173c
张宇赛 <zhangyvsai@163.com>
2021-01-18 11:56:03 +0800

    自定义绑定端口地址

--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"jRebel-license-server/handler"
 	"net/http"
 )
 
 func main() {
+	var host string
+	var port string
+	flag.StringVar(&port, "p", "9000", "端口,默认为9000")
+	flag.StringVar(&host, "h", "0.0.0.0", "绑定host,默认为0.0.0.0")
+	flag.Parse()
 	//1.注册处理器函数
 	http.HandleFunc("/uuid", handler.UUID)
 	http.HandleFunc("/jrebel/leases", handler.Leases)
 	http.HandleFunc("/jrebel/leases/1", handler.Leases1)
 	http.HandleFunc("/jrebel/validate-connection", handler.ValidateConnection)
-	fmt.Println(`
-    启动成功 端口号:9000
+	fmt.Printf(`
+    启动成功 端口号:%s
 	GET /uuid 生成随机串
-	http://localhost:9000/{uuid} 放入jrebel激活地址栏`)
-	err := http.ListenAndServe("127.0.0.1:9000", nil)
+	http://%s:%s/{uuid} 放入jrebel激活地址栏`, port, host, port)
+	err := http.ListenAndServe(host+":"+port, nil)
 	if err != nil {
 		fmt.Printf("http.ListenAndServe()函数执行错误,错误为:%v\n", err)
 		return
